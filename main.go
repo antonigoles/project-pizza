1d6694636d566a164e517465938fe34e9ced357a
wiktorka0614 <55910497+wiktorka0614@users.noreply.github.com>
2021-09-18 18:26:33 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
 func main() {
+  
+  r := handlers.Endpoints()
+	fmt.Println("Server is running..")
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatalln("sth went wrong sry")
+	}
 
 }
